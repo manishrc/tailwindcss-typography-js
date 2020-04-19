975afef8b2e29c36e866f47c575b3055ce5d9fbe
Manish Chiniwalar <hi@manishrc.com>
2020-04-19 16:36:52 +0200

    Fix typo

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const Typography = require("typography");
 const plugin = require("tailwindcss/plugin");
 
 const DEFAULT_CONFIG = {
-	includeNormalize: false
+	includeNormalize: false,
 };
 
-module.exports = function(config) {
-	const theme = new Typography(Object.assign(DEFAULT_CONFIG, config););
+module.exports = function (config) {
+	const theme = new Typography(Object.assign(DEFAULT_CONFIG, config));
 
-	return plugin(function({ addBase }) {
+	return plugin(function ({ addBase }) {
 		addBase(theme.toJSON());
 	});
 };
